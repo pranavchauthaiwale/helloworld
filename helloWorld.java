e05f2c8b1d951a4aae74171049269df2f22c8686
Pranav Chauthaiwale <pranavchauthaiwale@gmail.com>
2017-03-12 20:01:09 -0400

    helloworld java

--- helloWorld.java
+++ helloWorld.java
@@ -3,7 +3,6 @@ public class helloWorld
     public static void main(String args[])
     {
         System.out.println("HelloWorld");
-        System.out.println("One more line to print");
         
     }
 }
